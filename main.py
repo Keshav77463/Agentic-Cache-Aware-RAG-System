460cf2f25947dcba938879d3f8e62e8e85e4deb8
Keshav77463 <keshavsachdevarajp@gmail.com>
2026-03-30 04:05:34 +0530

    updating main.py

--- main.py
+++ main.py
@@ -1,21 +1,6 @@
-from src.utils.chunking import chunk_text
-import pandas as pd
-from src.embeddings.embedder import create_vectorstore
+from src.retriever.vector_store import load_vector_store
 
-# load correct file
-df = pd.read_csv("data/cleaned_reviews.csv")
+print("Loading vector store...")
+vector_store = load_vector_store()
 
-all_chunks = []
-
-# chunking
-for text in df['text']:
-    chunks = chunk_text(text)
-    all_chunks.extend(chunks)
-
-print("Total chunks:", len(all_chunks))
-print(all_chunks[:5])
-
-# ✅ pass chunks (NOT full text)
-vector_store = create_vectorstore(all_chunks)
-
-print("Vector store created successfully")
+print("Vector store loaded successfully")
