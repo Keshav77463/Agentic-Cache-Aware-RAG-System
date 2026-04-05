3bc8dea66227dc8c555834da1a54e7fb55bf6cf0
Keshav77463 <keshavsachdevarajp@gmail.com>
2026-04-05 21:59:54 +0530

    seperating rag pipeline from main.py

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-from src.retriever.vector_store import load_vector_store
+from src.pipeline.rag_pipeline import RAGPipeline
 
-print("Loading vector store...")
-vector_store = load_vector_store()
+pipeline = RAGPipeline()
 
-print("Vector store loaded successfully")
+while True:
+    query = input("\nEnter your query: ")
+    if query.lower() in ["exit", "quit"]:
+        print("Goodbye ")
+        break
+
+    answer = pipeline.run(query)
+    print("\nFinal Answer:")
+    print(answer)
